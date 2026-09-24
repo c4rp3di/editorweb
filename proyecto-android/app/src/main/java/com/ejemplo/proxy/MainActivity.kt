f7eebc3d60fb0b371758d30b3011d6c082c96c38
c4rp3di <c4rp3.di@gmail.com>
2026-09-24 03:28:47 +0200

    Actualiza proyecto-android/app/src/main/java/com/ejemplo/proxy/MainActivity.kt

--- proyecto-android/app/src/main/java/com/ejemplo/proxy/MainActivity.kt
+++ proyecto-android/app/src/main/java/com/ejemplo/proxy/MainActivity.kt
@@ -39,7 +39,7 @@ class MainActivity : AppCompatActivity() {
         prefs = getSharedPreferences("datos_app", MODE_PRIVATE)
         // [ONCREATE:FUNCIONALIDADES]
         setContentView(R.layout.activity_main)
-if (savedInstanceState == null) mostrarPantallaWebFragment()
+        if (savedInstanceState == null) mostrarPantallaWebFragment()
         // [ONCREATE_FIN:FUNCIONALIDADES]
         AppLogic.onIniciar(this)
     }
